d34e276b7e97f6002f75837b915e531e06e60d81
João Marcos Dei Tós <joao@ibglbrasil.com.br>
2023-10-27 11:54:42 -0300

    keyboard aplicado

---
+++ js/keyboard.js
@@ -0,0 +1,20 @@
+function insertChar(char) {
+  var display = document.getElementById("display");
+  display.textContent += char;
+}
+
+function backspace() {
+  var display = document.getElementById("display");
+  display.textContent = display.textContent.slice(0, -1);
+}
+
+function send() {
+  var display = document.getElementById("display");
+  var conteudo = display.textContent;
+
+  // Aqui você pode enviar o conteúdo para onde precisar
+  sessionStorage.setItem("Nome ddo clieente: ", conteudo);
+  // Substitua o console.log acima com a lógica real de envio
+
+  window.location.href = "pagamento.html"; // redireciona para proxima pagina desejada
+}
